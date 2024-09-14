cce8958bcd66b6efcb3bf4a7517b1ab9cdefb58e
samuelbasnet <samueallal743@gmail.com>
2024-09-14 13:53:18 +0545

    Update voting.c

--- voting.c
+++ voting.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
-int main()
-{int age, country;
+int main() {
+    // Voting eligibility
+    int age;
+    char str[] = "";
+    printf("Enter your Age: ");
+    scanf("%d",&age);
+    printf("Enter your country name: ");
+    scanf("%s",&str);
     
-   
-   
-   
+    if(age >= 18){
+        printf(" You are elgible to vote in %s!",str);
+    }else{
+        printf(" You arenot eligible to vote in %s",str);
+    }
+
+    return 0;
 }
